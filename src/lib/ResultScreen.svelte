93e7e5bca6770f342dbf2e4d306f41d423a1db7b
Luke Miller <76447172+LMiller-TZ@users.noreply.github.com>
2026-05-06 11:52:06 -0400

    Update ResultScreen.svelte

--- src/lib/ResultScreen.svelte
+++ src/lib/ResultScreen.svelte
@@ -27,7 +27,7 @@
   // NEW
   topThreeNatures = orderedNatures.slice(0, 3).map(nature => ({
     name: nature,
-    score: Math.round(store.weightedPoints[nature] * 100)
+    score: parseInt(store.weightedPoints[nature] * 100)
   }));
 
   for (let nature of orderedNatures) {
